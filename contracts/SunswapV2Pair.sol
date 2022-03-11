bd7382fc41dd7d8344c3798798e135f8036d961e
suniodev <95281585+suniodev@users.noreply.github.com>
2022-03-11 18:25:05 +0800

    Update SunswapV2Pair.sol

--- contracts/SunswapV2Pair.sol
+++ contracts/SunswapV2Pair.sol
@@ -168,7 +168,7 @@ contract SunswapV2Pair is ISunswapV2Pair, SunswapV2ERC20 {
             require(to != _token0 && to != _token1, 'SunswapV2: INVALID_TO');
         if (amount0Out > 0) TransferHelper.safeTransfer(_token0, to, amount0Out); // optimistically transfer tokens
         if (amount1Out > 0) TransferHelper.safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
-            if (data.length > 0) ISunswapV2Callee(to).SunswapV2Call(msg.sender, amount0Out, amount1Out, data);
+            if (data.length > 0) ISunswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
         balance0 = IERC20(_token0).balanceOf(address(this));
         balance1 = IERC20(_token1).balanceOf(address(this));
         }
